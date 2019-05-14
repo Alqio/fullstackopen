5f827ae88ac4be930e866acbdfbe2ef3c8fe3bc7
alqio <kyosti.alkio@gmail.com>
2019-05-14 11:10:30 +0300

    osa2 puhelinluettelo v4

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from 'react'
-import axios from 'axios'
 import numbersService from './numbersService'
 
 const App = () => {
@@ -15,6 +14,8 @@ const App = () => {
     const [newName, setNewName] = useState('');
     const [newNumber, setNewNumber] = useState('');
     const [newFilter, setNewFilter] = useState('');
+    const [message, setMessage] = useState(null);
+    const [color, setColor] = useState("green");
 
     const handlePersonChange = (event) => {
         setNewName(event.target.value);
@@ -29,35 +30,96 @@ const App = () => {
     const filteredPersons = () => persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()));
 
     const deleteNumber = id => {
-        numbersService.deleteNumber(id);
-        //deletoi frontista kanssa, eti id ja tee jotai 
+
+        const person = persons.find(p => p.id === id);
+
+        const del = window.confirm("Poistetaanko " + person.name + "?");
+
+        if (del) {
+
+            const name = person.name;
+
+            numbersService
+                .deleteNumber(id)
+                .then(data => {
+                    const newPersons = persons.filter(p => p.id !== id);
+                    setPersons(newPersons);
+                    createMessage("green", "Poistettiin " + name + " kannasta");
+                })
+                .catch(error => {
+                    createMessage("red", `Henkilöä ei enää löydy kannasta`);
+                    console.log(error);
+                    numbersService
+                        .getAll()
+                        .then(data => {
+                            setPersons(data)
+                        });
+                });
+
+        }
+
+    };
+
+    const clearMessage = () => setTimeout(() => setMessage(null), 3500);
+
+    const createMessage = (c, m) => {
+        setColor(c);
+        setMessage(m);
+        clearMessage();
     };
 
     const addPerson = (event) => {
         event.preventDefault();
-
         if (persons.find(person => person.name === newName)) {
-            alert(newName + " on jo luettelossa.");
+            //alert(newName + " on jo luettelossa.");
+            const replace = window.confirm(`${newName} on jo luettelossa, korvataanko vanha numero?`);
+            if (replace) {
+                const oldPerson = persons.find(person => person.name === newName);
+                const updatedPerson = {...oldPerson, number: newNumber};
+                numbersService
+                    .update(oldPerson.id, updatedPerson)
+                    .then(data => {
+                        setPersons(persons.map(per => per.id !== oldPerson.id ? per : data));
+                        createMessage("green", "Muutettiin henkilön " + oldPerson.name + " numero");
+                    })
+                    .catch(error => {
+                        createMessage("red", `Henkilöä ei enää löydy kannasta`);
+                        console.log(error);
+                        numbersService
+                            .getAll()
+                            .then(data => {
+                                setPersons(data)
+                            });
+                    });
+
+            }
+
         } else {
             const person = {
                 name: newName,
                 number: newNumber
             };
-            numbersService.create(person).then(data => setPersons(persons.concat(data)));
-            setNewName('');
+            numbersService.create(person).then(data => {
+                setPersons(persons.concat(data));
+                //setNewName('');
+                createMessage("green", "Lisättiin " + person.name + " onnistuneesti kantaan");
+            });
         }
     };
 
-    const numbers = () => filteredPersons().map(person => <Person name={person.name} number={person.number} key={person.name} deleteNumber={() => deleteNumber(person.id)}/>);
+    const numbers = () => filteredPersons().map(person => <Person name={person.name} number={person.number}
+                                                                  key={person.name}
+                                                                  deleteNumber={() => deleteNumber(person.id)}/>);
 
 
     return (
         <div>
             <h2>Puhelinluettelo</h2>
-
+            <Notification message={message} color={color}/>
             <h4>Lisää uusi</h4>
 
-            <PersonForm addPerson={addPerson} handleNumberChange={handleNumberChange} handlePersonChange={handlePersonChange}/>
+            <PersonForm addPerson={addPerson} handleNumberChange={handleNumberChange}
+                        handlePersonChange={handlePersonChange}/>
             <br></br>
             <Filter handleFilterChange={handleFilterChange}/>
 
@@ -66,6 +128,27 @@ const App = () => {
         </div>
     )
 
+};
+
+const Notification = ({message, color}) => {
+
+    const style = {
+        color: color,
+        background: 'lightgrey',
+        fontSize: 20,
+        borderStyle: 'solid',
+        borderRadius: 5,
+        padding: 10,
+        marginBottom: 10,
+    };
+
+    if (message === null) {
+        return null
+    } else {
+        return (
+            <div style={style} className="notification">{message}</div>
+        )
+    }
 };
 
 const PersonForm = ({addPerson, handlePersonChange, handleNumberChange}) => {
@@ -85,7 +168,11 @@ const PersonForm = ({addPerson, handlePersonChange, handleNumberChange}) => {
 };
 
 const Person = ({name, number, deleteNumber}) => {
-    return <><p>{name} {number}</p> <button onClick={deleteNumber}>poista</button></>;
+    return <>
+        <p>{name} {number}
+            <button onClick={deleteNumber}>poista</button>
+        </p>
+    </>;
 };
 
 const Filter = ({handleFilterChange}) => {
