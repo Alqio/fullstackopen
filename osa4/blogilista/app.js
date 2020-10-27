27b70d02e334c8c25f3a6e3610576b0f7da3a334
alqio <kyosti.alkio@gmail.com>
2020-10-27 13:14:17 +0200

    4.2

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogRouter = require('./controllers/blogs')
+
+const mongoUrl = process.env.MONGODB_URI
+
+mongoose.connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+})
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs/', blogRouter)
+
+module.exports = app
