89b4765fe7c66470348ec9a4a206b80d460225fb
alqio <kyosti.alkio@gmail.com>
2020-11-09 18:41:58 +0200

    4.17 wip

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 router.get('', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', {username: 1, name: 1, id: 1})
     response.json(blogs)
 })
 
@@ -10,7 +11,14 @@ router.post('', async (request, response) => {
     if (!request.body.title || !request.body.url) {
         response.status(400).send('No title or url provided')
     } else {
-        const blog = new Blog(request.body)
+        const users = await User.find({})
+
+        const user = users.length !== 0 ? users[0].id : null
+
+        const blog = new Blog({
+            ...request.body,
+            user
+        })
 
         const result = await blog.save()
 
