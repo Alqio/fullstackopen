b621e85664bafb976d1721a68177075019b9ced5
alqio <kyosti.alkio@gmail.com>
2020-11-16 13:56:16 +0200

    5.7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const Blog = ({blog}) => (
-    <div>
-        {blog.title} {blog.author}
-    </div>
-)
+const Blog = ({blog}) => {
+
+    const [visible, setVisible] = useState(false)
+
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5
+    }
+
+    if (visible) {
+        return (
+            <div style={blogStyle}>
+                {blog.title}
+                <br/>
+                {blog.url}
+                <br/>
+                {blog.likes} <button>like</button>
+                <br/>
+                {blog.author}
+                <br/>
+                <button onClick={() => setVisible(!visible)}>hide</button>
+            </div>
+        )
+    }
+
+    return (
+        <div style={blogStyle}>
+            {blog.title} {blog.author} <button onClick={() => setVisible(!visible)}>view</button>
+        </div>
+    )
+}
 
 export default Blog
